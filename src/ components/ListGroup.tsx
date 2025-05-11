0ea1da6f578074598696ffeb06a14aaa6a915390
Lunarc-Seattle <994527498@qq.com>
2025-05-11 16:49:01 +0800

    11

--- src/ components/ListGroup.tsx
+++ src/ components/ListGroup.tsx
@@ -1,12 +1,51 @@
-function ListGroup() {
+import { MouseEvent, useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (itme: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // let items = ["new", "san", "toyko", "london"];
+  //   items = [];
+
+  // let selectedIndex = 0;
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // const [name, setName ] = useState('');
+
+  // const message = items.length === 0 ? <p>no itme </p> : null;
+  // or 以下可以传参数，不需要传参数 就用上面的
+  // const message = ()=>{
+  // reuturn {items.length ===0? <p>no itme </p>: null;;
+  //}
+
+  // event handler
+  const handleClick = (event: MouseEvent) => console.log("clicked", event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>no item </p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index), console.log("cliked", item);
+              onSelectItem(item);
+            }}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
   // command + d 可以一起选中所有的class
   // 如果没有自动格式化 代码，选择 view - command template - config
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import ListGroup from "./ components/ListGroup";
-function App(){
-  return <div><ListGroup></ListGroup></div>
-  //// 相当于调用 ListGroup() 或者更简写：<ListGroup />
+function App() {
+  let items = ["new", "san", "toyko", "london"];
+  const handSelectItem = (item:string) =>{
+    console.log(item);
+  }
+
+  return (
+    <div>
+      <ListGroup items={items} heading={"Cities"} onSelectItem={handSelectItem}/>
+    </div>
+  );
+  {
+    /* 相当于调用 ListGroup() 或者更简写：<ListGroup /> */
+  }
 }
 
 export default App;
